0b5b0dc3d856a137db43ea9b0e78f72a684bd608
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-06-19 13:03:35 -0500

    center table

--- src/components/PersonsTable.jsx
+++ src/components/PersonsTable.jsx
@@ -2,8 +2,8 @@ const PersonsTable = ({persons}) => {
   console.log(persons);
 
   return (
-    <section className="text-gray-400 bg-gray-900 body-font">
-        <div className="container px-5 py-24 mx-auto">
+    <section className="body-font px-4 pt-20">
+        <div className="container mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-white">Pacientes</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Listado de pacientes</p>

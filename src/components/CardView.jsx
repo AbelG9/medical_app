58e697e619fb1a6c30c1c94b331e241bffef76ca
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-06-30 23:07:57 -0500

    edited main card styles

--- src/components/CardView.jsx
+++ src/components/CardView.jsx
@@ -2,8 +2,8 @@ const CardView = (props) => {
   return (
     <>
       <section className="body-font relative">
-        <div className="container px-8 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-3/4 md:w-2/3 flex flex-col mx-auto w-full py-8 px-3 sm:px-8 mt-8 md:mt-6 sm:mt-4 bg-gray-900 text-white rounded-3xl">
+        <div className="container px-3 pt-2 pb-12 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-4/5 md:w-full flex flex-col mx-auto w-full py-8 px-3 sm:px-8 mt-8 md:mt-6 bg-gray-900 text-white rounded-3xl">
             {props.children}
           </div>
         </div>

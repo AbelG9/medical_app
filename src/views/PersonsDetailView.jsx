de76f368fa30ee7b57ae72246cd4a4beb3dce410
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-07-05 10:17:38 -0500

    added swal confirmation for edit persons

--- src/views/PersonsDetailView.jsx
+++ src/views/PersonsDetailView.jsx
@@ -2,10 +2,13 @@ import PersonsForm from "../components/PersonsForm";
 import { getPersonById, updatePerson } from "../services/personsService";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const PersonsDetailView = () => {
   const [data, setData] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const getData = async () => {
     const getData = await getPersonById(id);
@@ -22,8 +25,27 @@ const PersonsDetailView = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const updateData = await updatePerson(id, data);
-    console.log(updateData);
+    Swal.fire({
+      title: "Desea editar los datos de paciente?",
+      text: "Esta acción es irreversible!",
+      confirmButtonText: "Si, deseo editarlos",
+      showCancelButton: true,
+      cancelButtonText: "No, no deseo editarlos",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const response = await updatePerson(id, data);
+        if (response) {
+          Swal.fire(
+            "Se ha guardado los datos de paciente correctamente!",
+            "",
+            "success"
+          );
+          navigate("/persons");
+        }
+      } else if (result.isDenied) {
+        Swal.fire("No se ha editado nada!", "", "info");
+      }
+    });
   };
 
   useEffect(() => {

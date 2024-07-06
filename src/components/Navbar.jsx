9a62e92ebeee5e2335c5e2d066cdd8012c6e9fdf
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-07-05 23:34:00 -0500

    fixed navbar styles and added disclosure for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const user = null;
 
   return (
-    <Disclosure as="nav" className="bg-sky-800">
+    <Disclosure as="nav" className="bg-sky-900">
       {({ open }) => (
         <>
           <div className="mx-auto">
@@ -33,11 +33,11 @@ const Navbar = () => {
                 <div className="flex space-x-4">
                   <Link
                     to="/"
-                    className="hover:rounded-md px-3 py-2 text-sm hover:bg-gray-700 hover:text-blue-600"
+                    className="hover:rounded px-3 py-2 text-sm hover:bg-gray-600 hover:text-blue-950 hover:font-semibold"
                   >
                     Inicio
                   </Link>
-                  <Popover className="hover:rounded-md px-3 py-2 text-sm hover:bg-gray-700 hover:text-blue-600">
+                  <Popover className="hover:rounded px-3 py-2 text-sm hover:bg-gray-600 hover:text-blue-950 hover:font-semibold">
                     <PopoverButton>Pacientes</PopoverButton>
                     <PopoverPanel
                       anchor="bottom"
@@ -45,13 +45,13 @@ const Navbar = () => {
                     >
                       <Link
                         to="/persons"
-                        className="bg-gray-800 mt-3 py-2 pl-2 rounded w-32 mr-4 text-sm hover:bg-gray-700 hover:text-blue-600"
+                        className="bg-gray-800 mt-3 py-2 pl-2 rounded w-32 mr-4 text-sm hover:bg-gray-500 hover:text-blue-950 hover:font-semibold"
                       >
                         <button>Ver todos</button>
                       </Link>
                       <Link
                         to="/persons/new"
-                        className="bg-gray-800 mt-1 py-2 pl-2 rounded w-32 mr-4 text-sm hover:bg-gray-700 hover:text-blue-600"
+                        className="bg-gray-800 mt-1 py-2 pl-2 rounded w-32 mr-4 text-sm hover:bg-gray-500 hover:text-blue-950 hover:font-semibold"
                       >
                         <button>Nuevo paciente</button>
                       </Link>
@@ -97,11 +97,31 @@ const Navbar = () => {
             </div>
           </div>
           <DisclosurePanel className="sm:hidden bg-blue-950 text-gray-100">
-            <div className="my-1 px-2 pb-3 pt-2 hover:bg-gray-700 hover:text-blue-600">
+            <div className="bg-gray-900 my-1 px-2 pb-3 pt-2 hover:bg-gray-600 hover:text-blue-950 hover:font-semibold">
               <Link to="/">Inicio</Link>
             </div>
-            <div className="my-1 px-2 pb-3 pt-2 hover:bg-gray-700 hover:text-blue-600">
-              <Link to="/">Pacientes</Link>
+            <div className="bg-gray-900 my-1 px-2 pb-3 pt-2 hover:bg-gray-600 hover:text-blue-950 hover:font-semibold">
+              <Disclosure>
+                <DisclosureButton className="py-2">Pacientes</DisclosureButton>
+                <DisclosurePanel className="text-gray-500">
+                  <div className="bg-gray-800 my-1 px-2 pb-3 pt-2 hover:bg-gray-500 hover:text-blue-950 hover:font-semibold">
+                    <Link
+                      to="/persons"
+                      className="mt-3 py-2 pl-2 rounded w-32 mr-4 text-sm"
+                    >
+                      <button>Ver todos</button>
+                    </Link>
+                  </div>
+                  <div className="bg-gray-800 my-1 px-2 pb-3 pt-2 hover:bg-gray-500 hover:text-blue-950 hover:font-semibold">
+                    <Link
+                      to="/persons/new"
+                      className="mt-1 py-2 pl-2 rounded w-32 mr-4 text-sm"
+                    >
+                      <button>Nuevo paciente</button>
+                    </Link>
+                  </div>
+                </DisclosurePanel>
+              </Disclosure>
             </div>
           </DisclosurePanel>
         </>
